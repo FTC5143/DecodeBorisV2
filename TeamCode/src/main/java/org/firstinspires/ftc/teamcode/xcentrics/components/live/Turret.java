ce06f409474866734e0e93b45c477a77086a8106
Alex <bassbothitter@gmail.com>
2025-12-11 17:03:58 -0600

    fixed heading error and cleaned up the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/components/live/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/components/live/Turret.java
@@ -4,7 +4,6 @@ import com.bylazar.configurables.annotations.Configurable;
 import com.pedropathing.geometry.Pose;
 import com.qualcomm.hardware.dfrobot.HuskyLens;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -17,251 +16,236 @@ import org.firstinspires.ftc.teamcode.xcentrics.util.qus.DcMotorQUS;
 import org.firstinspires.ftc.teamcode.xcentrics.util.qus.ServoQUS;
 import org.firstinspires.ftc.teamcode.xcentrics.components.Component;
 import org.firstinspires.ftc.teamcode.xcentrics.robots.Robot;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.TurretD;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.TurretP;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.TurretI;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.aim;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.blueGoalTagID;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.canSpin;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.flyError;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.flyP;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.flyI;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.flyD;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.flyTolerance;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.kickPos;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.maxRotation;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.minRotation;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.redGoalTagID;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.safetyOff;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.safetyOn;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.servoRange;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.targetVelocity;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.ticksPerTurretRotation;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.turretError;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.turretHeading;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.turretOffset;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.turretTarget;
-import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.turretTolerance;
+
+import static org.firstinspires.ftc.teamcode.xcentrics.components.live.TurretConfig.*;
 
 @Configurable
-class TurretConfig{
-    public static double TurretP = 0;
-    public static double TurretI = 0;
-    public static double TurretD = 0;
-    public static double flyP = 0,flyI = 0,flyD = 0;
+class TurretConfig {
+    public static double TurretP = 0, TurretI = 0, TurretD = 0;
+    public static double flyP = 0, flyI = 0, flyD = 0;
+
     public static int targetVelocity = 0;
-    public static int redGoalTagID = 1,blueGoalTagID = 2;
+
+    public static int redGoalTagID = 1, blueGoalTagID = 2;
     public static int servoRange = 360;
-    public static double turretOffset = 0;
-    public static double kickPos = 0,safetyOff = 0, safetyOn = 1;
+
+    public static double turretOffset = 0;   // mech offset in DEGREES
+    public static double kickPos = 0, safetyOff = 0, safetyOn = 1;
+
     public static boolean aim = false;
-    public static double turretTolerance = 0,flyTolerance = 0;
+
+    public static double turretTolerance = 0, flyTolerance = 0;
     public static double turretError, flyError;
+
     public static boolean canSpin = true;
-    public static double turretHeading,ticksPerTurretRotation,turretTarget;
+
+    public static double turretHeading;   // FIELD RELATIVE degrees
+    public static double ticksPerTurretRotation;
+    public static double turretTarget;    // ENCODER TICKS target
+
     public static double maxRotation = 270, minRotation = 0;
 }
+
 public class Turret extends Component {
+
     private DcMotorEx fly;
     private DcMotorQUS turret;
+
     private HuskyLens huskyLens;
-    private ServoQUS hood1,hood2, kicker,safety;
+    private ServoQUS hood1, hood2, kicker, safety;
+
     private MiniPID pid;
     private LiveRobot robot;
+
     private Pose redGoal, blueGoal;
     private double distance;
-
-
 
     {
         name = "turret";
     }
-    public Turret(Robot robot,LiveRobot roobot) {
+
+    public Turret(Robot robot, LiveRobot roobot) {
         super(robot);
         this.robot = roobot;
     }
-    public void registerHardware(HardwareMap hardwareMap){
+
+    @Override
+    public void registerHardware(HardwareMap hardwareMap) {
         super.registerHardware(hardwareMap);
-        fly =           hardwareMap.get(DcMotorEx.class,"fly");
-        turret =        new DcMotorQUS(hardwareMap.get(DcMotorEx.class,"turret"));
-        huskyLens =     hardwareMap.get(HuskyLens.class,"hl1");
-        hood1 =         new ServoQUS(hardwareMap.get(Servo.class,"H1"));
-        hood2 =         new ServoQUS(hardwareMap.get(Servo.class,"H2"));
-        kicker =        new ServoQUS(hardwareMap.get(Servo.class,"kicker"));
-        safety =        new ServoQUS(hardwareMap.get(Servo.class,"safety"));
+
+        fly = hardwareMap.get(DcMotorEx.class, "fly");
+        turret = new DcMotorQUS(hardwareMap.get(DcMotorEx.class, "turret"));
+
+        huskyLens = hardwareMap.get(HuskyLens.class, "hl1");
+
+        hood1 = new ServoQUS(hardwareMap.get(Servo.class, "H1"));
+        hood2 = new ServoQUS(hardwareMap.get(Servo.class, "H2"));
+        kicker = new ServoQUS(hardwareMap.get(Servo.class, "kicker"));
+        safety = new ServoQUS(hardwareMap.get(Servo.class, "safety"));
     }
 
-    public void startup(){
+    @Override
+    public void startup() {
         super.startup();
         huskyLens.selectAlgorithm(HuskyLens.Algorithm.TAG_RECOGNITION);
+
         pid = new MiniPID(TurretP, TurretI, TurretD);
-        fly.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        fly.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
     }
 
-    public void update(OpMode opMode){
+    @Override
+    public void update(OpMode opMode) {
         super.update(opMode);
-        HuskyLens.Block[] blocks = huskyLens.blocks();
-        for(int i = 0; i < blocks.length;i++){
-            addData("Block: " + i,blocks[i].toString());
-            addLine("Block center: (" + blocks[i].x + "," + blocks[i].y + ")");
-        }
-        //update pid coeficents
-        pid.setPID(TurretP,TurretI,TurretD);
-        setFlyPid(flyP,flyI,flyD);
 
-        //update current turret heading
-        setHeading(turret.motor.getCurrentPosition());
-        // only aim when we are ready
-        if(aim) {
-            //update distance
-            getDistance();
+        pid.setPID(TurretP, TurretI, TurretD);
+        setFlyPID(flyP, flyI, flyD);
 
-            //update turret position
-            calcTurretHeading();
+        // Update turretHeading from encoder + robot heading
+        updateTurretHeading();
 
-            //update hood angle
-            calcHoodAngle();
-
-            //set target velocity
-            calcFlySpeed();
-        }
-        //stop turret from moving to prevent damage to the robot when not updating pid
-        else {
+        if (aim) {
+            computeDistance();
+            computeTurretTarget();
+            computeHoodAngle();
+            computeFlySpeed();
+        } else {
             turret.queue_power(0);
         }
 
-        //update qus
-        update();
+        updateQUS();
     }
 
-    public void updateTelemetry(Telemetry telemetry){
+    @Override
+    public void updateTelemetry(Telemetry telemetry) {
         super.updateTelemetry(telemetry);
 
-        addLine("Fly PID: P: " + flyP + ",I: " + flyI + ",D: " + flyD);
-        addData("Fly Velocity: ", fly.getVelocity());
-        addLine("Turret PID: P: " + TurretP + ",I: " + TurretI + ",D: " + TurretD);
-
+        addLine("Turret PID: P=" + TurretP + " I=" + TurretI + " D=" + TurretD);
+        addLine("Fly PID: P=" + flyP + " I=" + flyI + " D=" + flyD);
+        addData("Fly Velocity", fly.getVelocity());
+        addData("Turret Heading (°)", turretHeading);
+        addData("Turret Target (ticks)", turretTarget);
     }
 
-    //shutdown the turret
-    public void shutdown(){
-        super.shutdown();
-        setVelocity(0);
-        engageSafety();
+    // ------------------------
+    // CORE TURRET MATH
+    // ------------------------
+
+    private void updateTurretHeading() {
+        double ticks = turret.motor.getCurrentPosition();
+
+        // CLOCKWISE is POSITIVE → encoder ticks directly map to +degrees
+        double turretRelativeDeg = ticks * (360.0 / ticksPerTurretRotation);
+
+        // Pedro heading is in radians CCW+, convert to degrees
+        double robotHeadingDeg = Math.toDegrees(robot.follower.getPose().getHeading());
+
+        // FIELD RELATIVE TURRET HEADING
+        turretHeading = AngleUnit.normalizeDegrees(
+                robotHeadingDeg + (-turretRelativeDeg) + turretOffset
+        );
+        // (-turretRelativeDeg) because robot CCW+ but turret CW+
     }
 
-    private void setFlyPid(double p,double i,double d){
-        fly.setVelocityPIDFCoefficients(p,i,d,0);
-        fly.setVelocityPIDFCoefficients(p,i,d,0);
-    }
-    private void setVelocity(double velocity){
-        fly.setVelocity(velocity, AngleUnit.RADIANS);
-    }
+    private void computeTurretTarget() {
+        Pose pose = robot.follower.getPose();
 
-    //calc distance from tag
-    public void getDistance(){
-        if(robot.isRed()) {
-            distance = redGoal.distanceFrom(robot.follower.getPose());
-        } else {
-            distance = blueGoal.distanceFrom(robot.follower.getPose());
-        }
-    }
-    //calc hood angle
-    private void calcHoodAngle(){
-        //implament formula here
-    }
-    //calculate turret heading
-    private void calcTurretHeading() {
-
-        Pose robotPose = robot.follower.getPose();
         double targetRadians;
 
         if (robot.isRed()) {
             targetRadians = Math.atan2(
-                    redGoal.getY() - robotPose.getY(),
-                    redGoal.getX() - robotPose.getX());
+                    redGoal.getY() - pose.getY(),
+                    redGoal.getX() - pose.getX());
         } else {
             targetRadians = Math.atan2(
-                    blueGoal.getY() - robotPose.getY(),
-                    blueGoal.getX() - robotPose.getX());
+                    blueGoal.getY() - pose.getY(),
+                    blueGoal.getX() - pose.getX());
         }
 
-        setTarget(targetRadians);
+        double targetDegField = Math.toDegrees(targetRadians);  // field-relative angle to goal
+        double robotHeadingDeg = Math.toDegrees(pose.getHeading());
 
-        // PID output is power
-        double output = pid.getOutput(turretHeading, turretTarget);
+        // Convert field-relative → robot-relative → turret-relative
+        double targetDegTurret = AngleUnit.normalizeDegrees(
+                targetDegField - robotHeadingDeg - turretOffset
+        );
 
-        if (turretTarget >= minRotation && turretTarget <= maxRotation) {
-            turret.queue_power(output);
-        } else {
-            turret.queue_power(0);  // don't damage turret
-        }
+        // Convert turretDegrees → ticks
+        turretTarget = targetDegTurret * (ticksPerTurretRotation / 360.0);
+
+        // Apply rotation limits (in ticks)
+        double minTicks = minRotation * (ticksPerTurretRotation / 360.0);
+        double maxTicks = maxRotation * (ticksPerTurretRotation / 360.0);
+
+        turretTarget = Math.max(minTicks, Math.min(maxTicks, turretTarget));
+
+        double power = pid.getOutput(turret.motor.getCurrentPosition(), turretTarget);
+        turret.queue_power(power);
     }
-    //calculate the fly speed
-    private void calcFlySpeed() {
-        //implaent formula here
+
+    // ------------------------
+    // FLYWHEEL / HOOD (unchanged but cleaned)
+    // ------------------------
+
+    private void setFlyPID(double p, double i, double d) {
+        fly.setVelocityPIDFCoefficients(p, i, d, 0);
     }
-    //update qus
-    private void update(){
+
+    private void computeFlySpeed() {
+        // implement formula here
+    }
+
+    private void computeHoodAngle() {
+        // implement formula here
+    }
+
+    private void computeDistance() {
+        Pose pose = robot.follower.getPose();
+        distance = robot.isRed() ?
+                redGoal.distanceFrom(pose) :
+                blueGoal.distanceFrom(pose);
+    }
+
+    private void updateQUS() {
         turret.update();
         hood1.update();
         hood2.update();
         kicker.update();
         safety.update();
     }
-    //launch an artifact
-    public void launch(){
-        if(turretError < turretTolerance && flyError < flyTolerance){
-            if(safety.servo.getPosition() == safetyOff){
+
+    // ------------------------
+    // CONTROL COMMANDS
+    // ------------------------
+
+    public void launch() {
+        if (turretError < turretTolerance && flyError < flyTolerance) {
+            if (safety.servo.getPosition() == safetyOff) {
                 canSpin = false;
                 kicker.queue_position(kickPos);
             }
         }
     }
-    //reset kicker
-    public void resetKicker(){
+
+    public void resetKicker() {
         kicker.queue_position(0);
         canSpin = true;
     }
-    //allow for the turret to track the goal
-    public void aim(){
-        aim = true;
-    }
-    public void stopAim(){
-        aim = false;
-    }
-    //disengage the safety
-    public void disengageSafety(){
+
+    public void aim() { aim = true; }
+    public void stopAim() { aim = false; }
+
+    public void disengageSafety() {
         safety.queue_position(safetyOff);
     }
-    //engage the safety
-    public void engageSafety(){
+
+    public void engageSafety() {
         safety.queue_position(safetyOn);
     }
 
-    private void setHeading(double encoderTicks) {
-        // turret angle relative to the robot (deg)
-        double turretRelativeDeg = encoderTicks * (360.0 / ticksPerTurretRotation);
-
-        // robot field heading (Pedro heading is radians CCW+)
-        double robotHeadingDeg = Math.toDegrees(robot.follower.getPose().getHeading());
-
-        // combine, apply optional mechanical offset
-        turretHeading =
-                AngleUnit.normalizeDegrees(robotHeadingDeg + turretRelativeDeg + turretOffset);
+    @Override
+    public void shutdown() {
+        super.shutdown();
+        fly.setVelocity(0);
+        engageSafety();
     }
-
-    private void setTarget(double targetRadians) {
-
-        // angle (deg) from robot to goal (field relative)
-        double targetDeg = Math.toDegrees(targetRadians);
-
-        // convert to angle relative to turret’s 0° on the robot
-        targetDeg -= Math.toDegrees(robot.follower.getPose().getHeading());
-        targetDeg -= turretOffset;
-
-        // convert degrees → encoder ticks
-        turretTarget = targetDeg * (ticksPerTurretRotation / 360.0);
-    }
-
 }
