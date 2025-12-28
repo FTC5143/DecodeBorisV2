29279d9b1b147cb63ef3e6b521a296317259557d
robotics5143_crxcs <alex.stephens@beasaint.org>
2025-12-28 17:28:50 -0600

    commit before the new auto paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/Auto/comp/blue/big12blue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/Auto/comp/blue/big12blue.java
@@ -10,87 +10,125 @@ import org.firstinspires.ftc.teamcode.xcentrics.components.live.Spin;
 
 public class big12blue extends LiveAutoBase {
     public static double wait = 2;
+    //line endpoints
     private final Pose startPose = new Pose(27.5,131.6,r(144)),
                        scorePose1 = new Pose(58.5,85,r(90)),
-                       scorePose = new Pose(58.5,85,r(180));
+                       rotatePose = new Pose(44,85,r(180)),
+                       pickup1 = new Pose(15,85,r(180)),
+                       scorePose = new Pose(58.5,85,r(180)),
+                       pickup2 = new Pose(9.3,59,r(180)),
+                       emptyGatePose = new Pose(16.5,70,r(90)),
+                       pickup3 = new Pose(9,36,r(180)),
+                       parkPose = new Pose(47,76,r(180));
+
+    //control points for lines
+    private final Pose p21 = new Pose(56.6,50.8),
+                       p22 = new Pose(6,60.3),
+                       p23 = new Pose(37.2,59.1),
+                       g1 = new Pose(13.5,52.9),
+                       g2 = new Pose(34.2,63.3),
+                       p31 = new Pose(65.7,26),
+                       p32 = new Pose(36,44.6),
+                       p33 = new Pose(67,31);
+
     private final PathChain scorePreload = robot.follower.pathBuilder()
             .addPath(new BezierLine(startPose,scorePose1))
             .setLinearHeadingInterpolation(startPose.getHeading(),scorePose1.getHeading())
             .build();
     private final PathChain getFirstPattern = robot.follower.pathBuilder()
-            .addPath(new BezierLine(scorePose1,new Pose(44,85)))
-            .setLinearHeadingInterpolation(scorePose1.getHeading(),180)
+            .addPath(
+                    new BezierLine(scorePose1, rotatePose))
+            .setLinearHeadingInterpolation(scorePose1.getHeading(),rotatePose.getHeading())
+
             .addParametricCallback(0.5,robot.intake.intake())
-            .addPath(new BezierLine(new Pose(44,85),new Pose(15,85)))
+
+            .addPath(
+                    new BezierLine(rotatePose,pickup1)
+            )
             .setConstantHeadingInterpolation(180)
             .build();
     private final PathChain scoreFirstPattern = robot.follower.pathBuilder()
-            .addPath(new BezierLine(new Pose(15,85),scorePose))
+            .addPath(
+                    new BezierLine(pickup1,scorePose)
+            )
             .setConstantHeadingInterpolation(180)
+
             .addParametricCallback(0,robot.intake.stopIntake())
             .addPoseCallback(new Pose(43,85),robot.turret.aim(), 0.6)
+
             .build();
     private final PathChain getSecondPattern = robot.follower.pathBuilder()
             .addPath(new BezierCurve(scorePose,
-                    new Pose(56.600, 50.800),
-                    new Pose(5.902, 60.315),
-                    new Pose(37.200, 59.100),
-                    new Pose(9.300, 59.000)
+                    p21,
+                    p22,
+                    p23,
+                    pickup2
             ))
             .setConstantHeadingInterpolation(180)
+
             .addParametricCallback(0,robot.turret.stopAim())
             .addPoseCallback(new Pose(47.5,65.4),robot.intake.intake(),0.4)
+
             .build();
     private final PathChain emptyGate = robot.follower.pathBuilder()
             .addPath(
                     new BezierCurve(
-                            new Pose(9.300, 59.000),
-                            new Pose(13.500, 52.900),
-                            new Pose(34.230, 63.266),
-                            new Pose(16.500, 70.000)
+                            pickup2,
+                            g1,
+                            g2,
+                            emptyGatePose
                     )
             )
             .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(90))
+
             .addParametricCallback(0.1,robot.intake.stopIntake())
+
             .build();
     private final PathChain scoreSecondPattern = robot.follower.pathBuilder()
             .addPath(
-                    new BezierLine(new Pose(16.500, 70.000), new Pose(58.500, 85.000))
+                    new BezierLine(emptyGatePose,scorePose)
             )
-            .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(180))
+            .setLinearHeadingInterpolation(emptyGatePose.getHeading(), scorePose.getHeading())
+
             .addParametricCallback(0.5,robot.turret.aim())
+
             .build();
 
     private final PathChain getThirdPattern = robot.follower.pathBuilder()
             .addPath(
                     new BezierCurve(
-                            new Pose(58.500, 85.000),
-                            new Pose(65.700, 26.000),
-                            new Pose(36.000, 44.600),
-                            new Pose(67.000, 31.000),
-                            new Pose(9.000, 36.000)
+                            scorePose,
+                            p31,
+                            p32,
+                            p33,
+                            pickup3
                     )
             )
-            .setConstantHeadingInterpolation(Math.toRadians(180))
+            .setConstantHeadingInterpolation(pickup3.getHeading())
+
             .addParametricCallback(0,robot.turret.stopAim())
             .addPoseCallback(new Pose(49,37.2),robot.intake.intake(), 0.6)
+
             .build();
     private final PathChain scoreThirdPattern = robot.follower.pathBuilder()
             .addPath(
-                    new BezierLine(new Pose(9.000, 36.000), new Pose(58.800, 85.000))
+                    new BezierLine(pickup3,scorePose)
             )
-            .setConstantHeadingInterpolation(Math.toRadians(180))
+            .setConstantHeadingInterpolation(pickup3.getHeading())
+
             .addParametricCallback(0,robot.intake.stopIntake())
             .addParametricCallback(0.5,robot.turret.aim())
+
             .build();
     private final PathChain park = robot.follower.pathBuilder()
             .addPath(
-                    new BezierLine(new Pose(58.800, 85.000), new Pose(47.000, 76.000))
+                    new BezierLine(scorePose,parkPose)
             )
-            .setConstantHeadingInterpolation(Math.toRadians(180))
+            .setConstantHeadingInterpolation(parkPose.getHeading())
             .addParametricCallback(0,robot.turret.stopAim())
             .build();
     private static int pathState = 0;
+
     @Override
     public void on_init() {
         robot.follower.setStartingPose(startPose);
@@ -109,7 +147,8 @@ public class big12blue extends LiveAutoBase {
 
     @Override
     public void on_loop() {
-
+        autoPathUpdate();
+        robot.addData("PathState",pathState);
     }
     private double r(double d){
         return Math.toRadians(d);
@@ -194,5 +233,8 @@ public class big12blue extends LiveAutoBase {
                     break;
                 }
         }
+
     }
+
+
 }
