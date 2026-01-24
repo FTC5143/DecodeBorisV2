81da52cb93ed90a189c8433d83f76034801b97f9
robotics5143_crxcs <alex.stephens@beasaint.org>
2026-01-23 18:36:10 -0600

    moved to debug folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/TeleOp/Debug/DebugTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/TeleOp/Debug/DebugTeleOp.java
@@ -1,10 +1,12 @@
-package org.firstinspires.ftc.teamcode.xcentrics.OpModes.TeleOp;
+package org.firstinspires.ftc.teamcode.xcentrics.OpModes.TeleOp.Debug;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.xcentrics.OpModes.TeleOp.LiveTeleopBase;
+
 
 @TeleOp(name = "Debug")
-public class DebugTeleOp extends LiveTeleopBase{
+public class DebugTeleOp extends LiveTeleopBase {
     @Override
     public void on_init() {
 
