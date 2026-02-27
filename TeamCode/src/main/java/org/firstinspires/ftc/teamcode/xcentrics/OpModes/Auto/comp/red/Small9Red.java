4230fd83878ccc0a95b405e5ca72bbc1cc6fc052
Xcentrics <150195241+Xcentrics@users.noreply.github.com>
2026-02-27 09:19:05 -0600

    updated paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/Auto/comp/red/Small9Red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/xcentrics/OpModes/Auto/comp/red/Small9Red.java
@@ -1,24 +1,125 @@
 package org.firstinspires.ftc.teamcode.xcentrics.OpModes.Auto.comp.red;
 
+import static org.firstinspires.ftc.teamcode.xcentrics.robots.Robot.isRed;
+
+import com.bylazar.field.Style;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.pedroPathing.PedroDrawing;
 import org.firstinspires.ftc.teamcode.xcentrics.OpModes.Auto.LiveAutoBase;
 import org.firstinspires.ftc.teamcode.xcentrics.components.live.Turret;
+import org.firstinspires.ftc.teamcode.xcentrics.paths.auto.blue.FarBlueAutoPaths;
 import org.firstinspires.ftc.teamcode.xcentrics.paths.auto.red.SmallTriangle;
 import org.firstinspires.ftc.teamcode.xcentrics.paths.auto.smallTriangleBlue;
 
 @Autonomous(name = "Small Red Triangle 9 Artifact",group = "red")
 public class Small9Red extends LiveAutoBase {
 
-
-    SmallTriangle paths;
+    private static final Style robotLook = new Style(
+            "", "#3F51B5", 0.75
+    );
+    FarBlueAutoPaths paths;
+    public PathChain p1;
+    public PathChain p2;
+    public PathChain p3;
+    public PathChain p4;
+    public PathChain p5;
+    public PathChain p6;
+    public PathChain p7;
+    public PathChain p8;
     private int p = 0;
     @Override
     public void on_init() {
-        paths = new SmallTriangle( robot.follower, true);
+        paths = new FarBlueAutoPaths(robot.follower);
         robot.turret.far();
-        robot.follower.setStartingPose(new Pose(paths.Poses[0].getX(),paths.Poses[0].getY(),Math.toRadians(90)));
+        robot.follower.setStartingPose(new Pose(56.71148134046984, 8.514007076140375,Math.toRadians(90)));
+        PedroDrawing.init();
+        isRed = true;
+        Follower follower = robot.follower;
+        p1 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(87.289, 8.514),
+                                new Pose(81.966, 32.385),
+                                new Pose(94.837, 34.256)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(0))
+
+                .build();
+
+        p2 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(94.837, 34.256),
+
+                                new Pose(101.129, 34.484)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(0))
+
+                .build();
+
+        p3 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(101.129, 34.484),
+
+                                new Pose(129.484, 34.806)
+                        )
+                ).setTangentHeadingInterpolation()
+
+                .build();
+
+        p4 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(129.484, 34.806),
+
+                                new Pose(87.289, 8.514)
+                        )
+                ).setConstantHeadingInterpolation(Math.toRadians(0))
+
+                .build();
+
+        p5 = follower.pathBuilder().addPath(
+                        new BezierLine(
+                                new Pose(87.289, 8.514),
+
+                                new Pose(132.097, 21.419)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(325))
+
+                .build();
+
+        p6 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(132.097, 21.419),
+                                new Pose(130.468, 18.806),
+                                new Pose(133.290, 11.097)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(336), Math.toRadians(-80))
+
+                .build();
+
+        p7 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(133.290, 11.097),
+                                new Pose(113.484, 15.823),
+                                new Pose(87.290, 8.419)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(-80), Math.toRadians(0))
+
+                .build();
+
+        p8 = follower.pathBuilder().addPath(
+                        new BezierCurve(
+                                new Pose(87.290, 8.419),
+                                new Pose(96.774, 16.081),
+                                new Pose(132.452, 12.968)
+                        )
+                ).setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(90))
+
+                .build();
     }
 
     @Override
@@ -34,91 +135,80 @@ public class Small9Red extends LiveAutoBase {
     @Override
     public void on_loop() {
         update();
+        PedroDrawing.drawDebug(robot.follower);
+        robot.intake.intake();
     }
 
     private void update(){
-        switch(p) {
+        switch(p){
             case 0:
-                followPath(paths.gotoscorepose);
                 robot.turret.aim();
+                halt(1);
+                robot.turret.shoot3();
+                followPath(p1);
+                robot.turret.stopAim();
                 p++;
                 break;
 
             case 1:
                 if(!robot.follower.isBusy()){
-                    robot.turret.shoot3();
+                    followPath(p2);
                     p++;
                     break;
                 }
 
             case 2:
-                if (!robot.follower.isBusy()){
-                    followPath(paths.turntogetfirstpattern);
+                if(!robot.follower.isBusy()){
+                    followPath(p3);
                     p++;
-                    robot.turret.stopAim();
                     break;
                 }
 
             case 3:
                 if(!robot.follower.isBusy()){
-                    robot.intake.intake();
-                    followPath(paths.pickupfirstpattern,0.5);
+                    followPath(p4);
+                    robot.turret.aim();
                     p++;
                     break;
                 }
 
             case 4:
                 if(!robot.follower.isBusy()){
-                    followPath(paths.scorefirstpattern,1);
-                    robot.turret.aim();
+                    halt(1);
+                    robot.turret.shoot3();
+                    followPath(p5);
+                    robot.turret.stopAim();
                     p++;
                     break;
                 }
 
             case 5:
                 if(!robot.follower.isBusy()){
-                    robot.turret.shoot3();
+                    followPath(p6);
                     p++;
                     break;
                 }
 
             case 6:
                 if(!robot.follower.isBusy()){
-                    followPath(paths.gotosecondpattern);
-                    robot.turret.stopAim();
-                    robot.intake.stopIntake();
+                    robot.turret.aim();
+                    followPath(p7);
                     p++;
                     break;
                 }
 
             case 7:
                 if(!robot.follower.isBusy()){
-                    followPath(paths.pickupsecondpattern, 0.5);
-                    robot.intake.intake();
-                    p++;
-                    break;
-                }
-
-            case 8:
-                if(!robot.follower.isBusy()){
-                    followPath(paths.scoresecondpattern,1);
-                    robot.turret.aim();
-                    p++;
-                    break;
-                }
-
-            case 9:
-                if(!robot.follower.isBusy()){
+                    halt(1);
                     robot.turret.shoot3();
+                    followPath(p8);
+                    robot.turret.stopAim();
                     p++;
                     break;
                 }
 
-            case 10:
-                followPath(paths.park);
-                robot.turret.stopAim();
-                robot.intake.stopIntake();
-                p++;
+
+            default:
                 break;
         }
     }
